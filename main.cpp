4a7e51039078e11fe954413a8cce364f7d8e1f7a
Parsa Habibi <phabibi@sfu.ca>
2021-04-12 10:50:38 -0700

    done

--- main.cpp
+++ main.cpp
@@ -6,11 +6,15 @@
 #include "2DRectangles.cpp"
 
 int main(){
+    std::cout << "====================test 1 (bad rectangle with - width,height) =========================" << std::endl;
+    Point2d p_b(0.0,0.0);
+    Rectangle rec_b(p_b, -5.0 , -5.0 );
+    std::cout << "====================test 1 (good rectnalge) =========================" << std::endl;
     Point2d p(0.0,0.0);
     Rectangle rec(p, 5.0 , 5.0 );
-    
-    std::cout << "====================test 1=========================" << std::endl;
     std::cout << "rectangle = " << rec << std::endl;
+    
+    std::cout << "====================test 1 (is_contained invalid) =========================" << std::endl;
 
     //tries to set bottom left to -1,-1 (should be invalid) */
     Point2d p_test(-2.0,-2.0);
@@ -27,10 +31,10 @@ int main(){
 
         catch(const char* err)
         {
-            std::cout << err << std::endl; 
+            std::cout << err<< " point = " << p_test << std::endl; 
         }
 
-    std::cout << "====================test 2=========================" << std::endl;
+    std::cout << "====================test 2(point contained valid)=========================" << std::endl;
     Point2d p_test_1(2.0,1.0);
     try{
         if(rec.is_contained(p_test_1))
@@ -49,7 +53,7 @@ int main(){
         }
 
 
-    std::cout << "====================test 3=========================" << std::endl;
+    std::cout << "====================test 3 (rectangle collide) =========================" << std::endl;
     Point2d p_test_2(-2.0,-2.0);
     Rectangle rec2(p_test_2,40.0,40.0);
 
@@ -57,6 +61,45 @@ int main(){
         if(rec.is_intersected(rec2))
         {
             std::cout << "the rectangle : " << rec2 << " collided with the rectangle : " << rec << std::endl;
+        }
+        else 
+        {
+            throw("did not collide");
+        }
+    }
+
+        catch(const char* err)
+        {
+            std::cout << err << std::endl; 
+        }
+    std::cout << "====================test 4 (rectangle not collide) =========================" << std::endl;
+    Point2d p_test_3(-100.0,-100.0);
+    Rectangle rec3(p_test_3,40.0,40.0);
+
+    try{
+        if(rec.is_intersected(rec3))
+        {
+            std::cout << "the rectangle : " << rec3 << " collided with the rectangle : " << rec << std::endl;
+        }
+        else 
+        {
+            throw("did not collide");
+        }
+    }
+
+        catch(const char* err)
+        {
+            std::cout << err << std::endl; 
+        }
+
+    std::cout << "====================test 5 (collide with the same rectangle) =========================" << std::endl;
+    Point2d p_test_4(0.0,0.0);
+    Rectangle rec4(p_test_4,5.0,5.0);
+
+    try{
+        if(rec.is_intersected(rec4))
+        {
+            std::cout << "the rectangle : " << rec4 << " collided with the rectangle : " << rec << std::endl;
         }
         else 
         {
--- multiples.cpp
+++ multiples.cpp
@@ -10,15 +10,16 @@ unsigned int multiples(unsigned int n)
     //could be at most n thus space complexity would be O(n)
     std::vector<int> results(n , 1); 
 
-    /*the idea is to do a greedy algorithm that keeps track of what numbers we have seen so far. for exampl, 4. 2*2 and 8: 2*2*2 however we have already 
-     * seen 2*2 in the 4th position of our sequence thus by keeping track of what we have seen so far, we could reduce the time 
-     * furthermore, we know that for each iteration, the next number in our sequence is always the smallest number that we have see so far*the our 3 multipliers -> 
-     * min( 2*(previous_sequence) , 3*(previous_sequence) , 5*(previous_sequence) ). we first assume that our list only contains the trivial case which is 1
-     * our next number in the sequence can be determined from min(2*1, 3*1, 5*1) which would be 2. thus result[i] where i=1 && i->n = 2. now we check to see what
-     * which multiplier(2 or 3 or 5) we have taken and we increment its position pointer in our result array respectively. 
+    /*the idea is to do a greedy algorithm that keeps track of what numbers we have seen so far. for example, 4: 2*2 and 8: 2*2*2 however we have already 
+     * seen 2*2 in the 4th position of our sequence and we know that it was a multiple of 2. thus by keeping track of what we have seen so far for each 
+     * multiple(2,3,5). 
+     * furthermore, we know that for each iteration, the next number in our sequence is always the smallest number that we have see so far*the our 3 multipliers, thus
+     * by keeping track of which multiple was taken at which position, we eliminate the need to treverse all the way back the list to find it again. we would simply
+     * just check to see if the res.at(curr_pos) == res.at(curr_pos) * multiple , if true , we increment the pointer for that multiple. 
+     * Finally since our for loop goes from 1 to n, the time complexity of this algorithm is linear ( O(N) )
      */
 
-    /*pointers that corresponds to which */ 
+    /*multiples of .. */ 
     unsigned int m_2 = 2;
     unsigned int m_3 = 3;
     unsigned int m_5 = 5; 
